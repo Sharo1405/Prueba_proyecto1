ea12bc04f259a81b184f7584b13826cad5e4facc
sharolin <sharolin33@gmail.com>
2019-09-06 18:20:23 -0600

    select4
    listo

--- Server/AST/Expresiones/Select4.cs
+++ Server/AST/Expresiones/Select4.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Server.AST.BaseDatos;
 using Server.AST.Entornos;
 using Server.AST.Expresiones.TipoDato;
+using Server.AST.Instrucciones;
 using Server.AST.Otras;
 using static Server.AST.Expresiones.Operacion;
 
@@ -48,6 +49,7 @@ namespace Server.AST.Expresiones
                         if (basee.Tabla.TryGetValue(idTabla.ToLower(), out encontrado2))
                         {
                             String asterisco = "";
+                            Tabla paraRetorno = new Tabla();
                             if (OpcionSelect is Expresion)
                             {
                                 if (OpcionSelect is ListaExpresiones)
@@ -55,44 +57,168 @@ namespace Server.AST.Expresiones
                                     ListaExpresiones lexp = (ListaExpresiones)OpcionSelect;
                                     LinkedList<String> listacampos = new LinkedList<string>();
                                     LinkedList<Comas> lcomas = (LinkedList<Comas>) lexp.getValue(entorno, listas, management);
+
                                     foreach (Comas coma in lcomas)
                                     {
+
                                         if (coma.expresion1 is Identificador)
                                         {
                                             Identificador idd = (Identificador)coma.expresion1;
-                                            listacampos.AddLast(idd.id);
-                                        }
+                                            //listacampos.AddLast(idd.id);
 
+                                            foreach (KeyValuePair<string, Columna> kvp in encontrado2.columnasTabla)
+                                            {
+                                                Columna col = kvp.Value;
+                                                if (col.idColumna.ToLower().Equals(idd.id.ToLower()))
+                                                {
+                                                    paraRetorno.columnasTabla.Add(idd.id.ToLower(), col);
+                                                    break;
+                                                }
+                                            }
+
+                                        }
+                                        else if (coma.expresion1 is ListaPuntos)
+                                        {
+                                            ListaPuntos lpuntos = (ListaPuntos)coma.expresion1;
+                                            LinkedList<Puntos> ExpSeparadasPuntos = lpuntos.ExpSeparadasPuntos;
+                                            //posicion 0 es la columna y pos 1 es el id de elemento del usertype
+                                            String idColumnaPunto = "";
+                                            String idItemUserType = "";
+                                            int count = 0;
+                                            foreach (Puntos punto in ExpSeparadasPuntos)
+                                            {
+                                                if (count == 0)
+                                                {
+                                                    if (punto.expresion1 is Identificador)
+                                                    {
+                                                        idColumnaPunto = ((Identificador)punto.expresion1).id.ToLower();
+                                                    }
+                                                }
+                                                else
+                                                {
+                                                    if (punto.expresion1 is Identificador)
+                                                    {
+                                                        idItemUserType = ((Identificador)punto.expresion1).id.ToLower();
+                                                    }
+                                                }
+                                                count++;
+                                            }
+
+
+                                            //ya buscando en laas columnas
+                                            Columna colNuevita = new Columna();
+                                            LinkedList<object> listaItems = new LinkedList<object>();
+                                            Columna colNuevita2 = new Columna();
+                                            if (encontrado2.columnasTabla.TryGetValue(idColumnaPunto.ToLower(), out colNuevita))
+                                            {
+                                                CreateType extract = new CreateType();
+                                                foreach (CreateType type in colNuevita.valorColumna)
+                                                {
+                                                    foreach (itemType t in type.itemTypee) {
+                                                        if (t.id.ToLower().Equals(idItemUserType.ToLower())) {
+                                                            //LinkedList<itemType> ite = new LinkedList<itemType>();
+                                                            //ite.AddLast(t);
+                                                            //CreateType n = new CreateType(idColumnaPunto.ToLower(),ite, type.linea, type.columna);
+                                                            listaItems.AddLast(t);
+                                                        }
+                                                    }
+                                                }
+                                                colNuevita2.idColumna = colNuevita.idColumna + "." + idItemUserType;
+                                                colNuevita2.idTipo = colNuevita.idTipo;
+                                                colNuevita2.valorColumna = listaItems;
+                                                paraRetorno.columnasTabla.Add(colNuevita2.idColumna, colNuevita2);
+                                            }
+                                            else
+                                            {
+                                                listas.errores.AddLast(new NodoError(this.linea, this.columna, NodoError.tipoError.Semantico,
+                                                    "El id de la columna no existe en la tabla " + idTabla ));
+                                                return tipoDato.errorSemantico;
+                                            }
+
+                                        }
                                     }
 
                                     //aqui deberia empezar el select
-                                    Tabla paraRetorno = new Tabla();
-                                    int contador = 0;
+                                                                        
                                     paraRetorno.idTabla = encontrado2.idTabla;
-                                    paraRetorno.llavePrimaria = encontrado2.llavePrimaria;
-                                    foreach (String campo in listacampos) {
-                                        foreach (KeyValuePair<string, Columna> kvp in encontrado2.columnasTabla)
-                                        {
-                                            Columna col = kvp.Value;
-                                            if (col.idColumna.ToLower().Equals(campo.ToLower()))
-                                            {
-                                                paraRetorno.columnasTabla.Add(campo.ToLower(), col);
-                                            }
-                                        }
-                                    }
+                                    paraRetorno.llavePrimaria = encontrado2.llavePrimaria;                                    
 
                                     return paraRetorno;
                                 }
                                 else if (OpcionSelect is ListaPuntos)
                                 {
+                                    ListaPuntos lpuntos = (ListaPuntos)OpcionSelect;
+                                    LinkedList<Puntos> ExpSeparadasPuntos = lpuntos.ExpSeparadasPuntos;
+                                    //posicion 0 es la columna y pos 1 es el id de elemento del usertype
+                                    String idColumnaPunto = "";
+                                    String idItemUserType = "";
+                                    int count = 0;
+                                    foreach (Puntos punto in ExpSeparadasPuntos)
+                                    {
+                                        if (count == 0)
+                                        {
+                                            if (punto.expresion1 is Identificador)
+                                            {
+                                                idColumnaPunto = ((Identificador)punto.expresion1).id.ToLower();
+                                            }
+                                        }
+                                        else
+                                        {
+                                            if (punto.expresion1 is Identificador)
+                                            {
+                                                idItemUserType = ((Identificador)punto.expresion1).id.ToLower();
+                                            }
+                                        }
+                                        count++;
+                                    }
 
+
+                                    //ya buscando en laas columnas
+                                    Columna colNuevita = new Columna();
+                                    LinkedList<object> listaItems = new LinkedList<object>();
+                                    Columna colNuevita2 = new Columna();
+                                    if (encontrado2.columnasTabla.TryGetValue(idColumnaPunto.ToLower(), out colNuevita))
+                                    {
+                                        CreateType extract = new CreateType();
+                                        foreach (CreateType type in colNuevita.valorColumna)
+                                        {
+                                            foreach (itemType t in type.itemTypee)
+                                            {
+                                                if (t.id.ToLower().Equals(idItemUserType.ToLower()))
+                                                {
+                                                    listaItems.AddLast(t);
+                                                }
+                                            }
+                                        }
+                                        colNuevita2.idColumna = colNuevita.idColumna + "." + idItemUserType;
+                                        colNuevita2.idTipo = colNuevita.idTipo;
+                                        colNuevita2.valorColumna = listaItems;
+                                        paraRetorno.columnasTabla.Add(colNuevita.idColumna, colNuevita2);
+                                    }
+                                    else
+                                    {
+                                        listas.errores.AddLast(new NodoError(this.linea, this.columna, NodoError.tipoError.Semantico,
+                                            "El id de la columna no existe en la tabla " + idTabla));
+                                        return tipoDato.errorSemantico;
+                                    }
+
+                                    return paraRetorno;
                                 }
-                                else
+                                else if(OpcionSelect is Identificador)
                                 {
-
+                                    Identificador ii = (Identificador)OpcionSelect;
+                                    foreach (KeyValuePair<string, Columna> kvp in encontrado2.columnasTabla)
+                                    {
+                                        Columna col = kvp.Value;
+                                        if (col.idColumna.ToLower().Equals(ii.id.ToLower()))
+                                        {
+                                            paraRetorno.columnasTabla.Add(col.idColumna.ToLower(), col);
+                                            break;
+                                        }
+                                    }
+                                    return paraRetorno;
                                 }
-
-
+                                return null;
                             }
                             else
                             {
